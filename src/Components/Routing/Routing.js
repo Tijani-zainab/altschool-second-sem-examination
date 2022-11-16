f0b876693789c1be8883af2ed4861f1173d12839
Zainab Tijani <jani.tijj@gmail.com>
2022-11-16 01:38:13 +0100

    comment out foote route, showing on every page

--- src/Components/Routing/Routing.js
+++ src/Components/Routing/Routing.js
@@ -6,7 +6,7 @@ const Routing = () => {
         <Routes>
             {/* <Route path='/' element={<Landing />} /> */}
             <Route path='/projects' element={<Projects />} />
-            <Route path='*' element={<NotFound />} />
+            {/* <Route path='*' element={<NotFound />} /> */}
         </Routes>
     );
 };
