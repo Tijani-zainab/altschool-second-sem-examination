bc27bc9f4dd388151700a6878f88a8dd26484e05
Zainab Tijani <jani.tijj@gmail.com>
2023-01-11 18:58:26 +0100

    url fix

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,8 +9,8 @@ const About = () => {
 
                 <p>what I am up to:</p>
 
-                I am currently pursuing my diploma in frontend engineering at AltSchool Africa.
-                I am member at waterthetrees open sourced tree mapping and maintainance project for saving the planet.
+                I am currently pursuing my diploma in frontend engineering at <a className="about--link" href="https://www.altschoolafrica.com/">AltSchool Africa.</a>
+                I am member at <a className="about--link" href="http://waterthetrees.com/">waterthetrees</a> open sourced tree mapping and maintainance project for saving the planet.
                 I am member of the community directors for code for san francisco open source
             </p>
         </div>
