18d8b34a02a2044322abf00e7ce5b0a34101adca
Zainab Tijani <jani.tijj@gmail.com>
2023-02-26 18:58:32 +0100

    solved leetcode problems

---
+++ src/Components/Leetcode-Practice/Leetcode.js
@@ -0,0 +1,96 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+const isPalindrome = (x) => {
+    const xString = x.toString();
+    const xStringReversed = xString.split('').reverse().join('');
+    return xString === xStringReversed;
+};
+
+
+//2
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+// Constraints:
+
+// The number of nodes in each linked list is in the range [1, 100].
+// 0 <= Node.val <= 9
+// It is guaranteed that the list represents a number that does not have leading zeros.
+
+// Definition for singly-linked list.
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+
+
+const addTwoNumbers = (l1, l2) => {
+    let sum = 0;
+    let carry = 0;
+    let head = new ListNode(0);
+    let current = head;
+    while (l1 || l2 || carry) {
+        sum = 0;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        if (carry) {
+            sum += carry;
+        }
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+    return head.next;
+};
+
+
+
+
+
