ac00852e7a15e9c0eb4ba36dcec0c383b5c480bd
Zainab Tijani <jani.tijj@gmail.com>
2023-02-27 23:58:43 +0100

    further leetcode problems

--- src/Components/Leetcode-Practice/Leetcode.js
+++ src/Components/Leetcode-Practice/Leetcode.js
@@ -90,7 +90,34 @@ const addTwoNumbers = (l1, l2) => {
     return head.next;
 };
 
+//3
 
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+    const reverse = (x) => {
+        const reversed = parseInt(x.toString().split('').reverse().join('')) * Math.sign(x);
+        return (reversed > 2147483647 || reversed < -2147483648) ? 0 : reversed;
+    };
 
